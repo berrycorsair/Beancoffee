24145c22a9ec3d4c00466ac1a3f62902ea6c43a5
Matthew Berry <m.s.berry@outlook.com>
2026-06-02 19:24:49 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import RibbonTicker from "./components/RibbonTicker";
+import NavBar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
+import CtaSection from "./components/CtaSection";
+import FeaturesSection from "./components/FeaturesSection";
+import ProductShowcase from "./components/ProductShowcase";
+import FooterSection from "./components/FooterSection";
+import AboutSection from "./components/AboutSection";
+import ContactSection from "./components/ContactSection";
+
+export default function App() {
+  return (
+    <div className="app">
+      {/* NAVBAR *>}
+      <NavBar />
+
+      {/* HERO */}
+      <section className="hero bg-hero">
+        <div className="hero-grid">
+          <HeroSection />
+        </div>
+      </section>
+      
+      <RibbenTicker />
+
+      {/* FEATURES / CAROUSEL */}
+      <section className="features bg-features" id="shop">
+        <FeaturesSection />
+      </section>
+
+      {/* PRODUCT SHOWCASE */}
+      <section className="bg-cta">
+        <ProductShowcase />
+      </section>
+
+      <RibbonTicker />
+
+      {/* CTA */}
+      <section className="bg-cta">
+        <CtaSection />
+      </section>
+
+      {/* ABOUT */}
+      <section className="bg-cta" id="about">
+        <AboutSection />
+      </section>
+
+      {/* CONTACT */}
+      <section className="bg-cta" id="contact">
+        <ContactSection />
+      </section>
+
+      {/* FOOTER */}
+      <section className="bg-footer">
+        <FooterSection />
+        </section>
+    </div>
+  );
+}
