17dbee5041c036cded6ac181116552abd31e199a
Matthew Berry <m.s.berry@outlook.com>
2026-06-03 16:57:00 -0700

    modified NavBar

---
+++ src/components/ProductShowcase.jsx
@@ -0,0 +1,7 @@
+import { motion } from "framer-motion";
+    import ScrollReveal, { StaggerContainer, StaggerItem } from "./ui/ScrollReveal";
+    import Badge from "./ui/Badge";
+    import Button from "./ui/Button";
+    import Separator from "./ui/"
+    
+}
